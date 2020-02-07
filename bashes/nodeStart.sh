6672889857ce3341210d33834a93efa4dc4adc14
lukaqwe <alexeluca.spataru@gmail.com>
2020-02-07 12:07:53 +0200

    Update README

--- bashes/nodeStart.sh
+++ bashes/nodeStart.sh
@@ -1,1 +1,1 @@
-geth --datadir=./data/ --networkid 4567 --rpc --rpcport $2 --port $3 --rpcaddr 127.0.0.1 --nodiscover --ipcpath=./data/geth.ipc --rpccorsdomain "*" --rpcapi admin,eth,web3,personal,net console 2> logs/node.log
+geth --datadir=./data/ --networkid 4567 --rpc --rpcport $2 --port $3 --rpcaddr 127.0.0.1 --allow-insecure-unlock --ipcpath=./data/geth.ipc --rpccorsdomain "*" --rpcapi admin,eth,web3,personal,net console 2> logs/node.log
